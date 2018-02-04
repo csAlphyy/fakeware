f333113d408d348da56b2ff2d9b0e7b6d3b261d9
csAlphyy <31821288+csAlphyy@users.noreply.github.com>
2018-02-04 13:14:01 +0000

    fixed esp

--- Core/PaintTraverse.cpp
+++ Core/PaintTraverse.cpp
@@ -20,33 +20,8 @@ void __stdcall Hooks::hkPaintTraverse(vgui::VPANEL panel, bool forceRepaint, boo
 	}
 	else if (panelId == panel)
 	{
-	//	Visuals::Get().PaintTraverse();
-		for (int i = 0; i < g_EntityList->GetHighestEntityIndex(); i++) {
-			C_BasePlayer* entity = C_BasePlayer::GetPlayerByIndex(i);
-
-			if (!entity) continue;
-			if (entity->IsDormant() || entity->m_iHealth() <= 0) continue;
-
-			RECT bbox = getPlayerBox(entity);
-
-			g_VGuiSurface->DrawSetColor(0, 0, 0, 255);
-			g_VGuiSurface->DrawOutlinedRect(bbox.left - 1, bbox.top - 1, bbox.right + 1, bbox.bottom + 1);
-			g_VGuiSurface->DrawOutlinedRect(bbox.left + 1, bbox.top + 1, bbox.right - 1, bbox.bottom - 1);
-
-			g_VGuiSurface->DrawSetColor(255, 255, 255, 255);
-			g_VGuiSurface->DrawOutlinedRect(bbox.left, bbox.top, bbox.right, bbox.bottom);
-		}
+		Visuals::Get().PaintTraverse();
 		grenade_prediction::Get().Paint();
-		/*if (g_LocalPlayer && g_LocalPlayer->IsAlive()) {
-			float damage;
-
-			int xs, ys;
-			g_EngineClient->GetScreenSize(xs, ys);
-			xs /= 2; ys /= 2;
-
-			g_VGuiSurface->DrawSetColor(trace_autowallable(damage) ? Color(130, 241, 13) : Color(255, 102, 102));
-			g_VGuiSurface->DrawOutlinedCircle(xs - 2, ys - 2, 5, 5);
-		}*/
 
 
 		Menu::Get().PaintTraverse();	
